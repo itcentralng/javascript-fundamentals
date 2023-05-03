dfe22b3c61acbf51ad327cae69ab1b107c82ee70
Nasir Mustapha <teboy.com@gmail.com>
2023-05-03 13:32:53 +0100

    more on strings

--- datatypes/strings.js
+++ datatypes/strings.js
@@ -2,6 +2,34 @@ name = "Ahmad "
 name1 = "Amrah "
 name2 = "Jamil "
 _nma = 'Kabiru '
-FirstName = 'Rasheedat '
+FirstName = 'Rasheedat Jamiu Atinuke'
 
 console.log('my name is '+name+name1)
+
+console.log(FirstName.length)
+
+console.log(FirstName.slice(0, 4))
+console.log(FirstName.slice(2, 5))
+console.log(FirstName.slice(6, 9))
+
+console.log(FirstName.substring(-3, 6))
+
+console.log(FirstName.substr(2, 6))
+console.log(FirstName.substr(2, 3))
+console.log(FirstName.substr(6, 3))
+
+console.log(FirstName.replace('Jamiu', 'Ahmad'))
+console.log(FirstName.replace('a', 'o'))
+console.log(FirstName.replaceAll('a', 'o'))
+
+console.log(FirstName.replace('Atinuke', 'Balaraba'))
+console.log(FirstName.replace('at', 'u'))
+
+// Class Work
+
+// 1. Create 5 string examples
+// 2. Using slice get the string again removing its first and last characters
+// 3. Using substring get the string again removing its first and last characters
+// 4. Using substr get the string again removing its first and last characters
+// 5. Using replace, change the first occurance of 'a' with 'x'
+// 6. Using replaceAll, change all occurances of 'a' with 'x'
