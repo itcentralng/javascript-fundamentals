608de87ba535444c4d89ae133e891572088f7bda
amrah <Saliamrah300@gmail.com>
2023-05-04 11:18:22 +0100

    classwork1

--- amrah/datatypes/strings.js
+++ amrah/datatypes/strings.js
@@ -1,5 +1,45 @@
-name = "amrah"
-name1 = "sali" 
+//names = "amrah"
+//name1 = "sali" 
 
-console.log(name + name1)
+//console.log(name + name1)
+
+
+// Class Work
+
+// 1. Create 5 string examples
+// 2. Using slice get the string again removing its first and last characters
+// 3. Using substring get the string again removing its first and last characters
+// 4. Using substr get the string again removing its first and last characters
+// 5. Using replace, change the first occurance of 'a' with 'x'
+// 6. Using replaceAll, change all occurances of 'a' with 'x'
+
+//1
+
+name = "amrah";
+age = "30";
+color = "green";
+height = "5'5"
+school = "naub"
+
+//2
+
+console.log(name.slice(1, 4))
+
+//3
+
+console.log(color.substring(1, 4))
+
+//4
+console.log(school.substr(1, 2))
+
+//5
+console.log(name.replace('a', 'x'))
+
+//6
+console.log(name.replaceAll('a', 'x'))
+
+
+
+
+
 
