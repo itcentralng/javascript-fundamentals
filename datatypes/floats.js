2fdee0c950323c0bdaa6eb24129c4279ed119061
Nasir Mustapha <teboy.com@gmail.com>
2023-05-05 11:32:36 +0100

    datatypes conclusion

--- datatypes/floats.js
+++ datatypes/floats.js
@@ -3,3 +3,6 @@ num2 = 6.4
 num3 = -1.6
 
 console.log(num2+num3)
+
+console.log(parseFloat('2.5'+1))
+console.log(parseFloat(2.5+1))
--- datatypes/integers.js
+++ datatypes/integers.js
@@ -1,4 +1,8 @@
-num1 = -1
-num2 = 100
+num1 = 50
+num2 = 95
 
 console.log(num1+num2)
+
+console.log(num2.toString())
+
+console.log(parseInt('50'))
